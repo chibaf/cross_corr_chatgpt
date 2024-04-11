469f801c8f3dc46241df83842ac1cb69ce78e37b
chibaf <chibaf@mac.com>
2024-04-11 22:32:30 +0900

    Add files via upload

--- cross_corr_numpy.py
+++ cross_corr_numpy.py
@@ -7,14 +7,14 @@ from numpy import genfromtxt # csv reading
 #data_A = np.array([0, 0, 1, 0, 0, 1, 0, 0, 1, 0])
 #data_B = np.array([0, 0, 0, 1, 0, 0, 1, 0, 0, 1])
 # read csv with numpy
-a=genfromtxt(sys.argv[1], delimiter=',')
-at=np.transpose(a)
+a=genfromtxt(sys.argv[1], delimiter=',') # CSVデータの読み込み 1レコード＝Tc x 10 , 10レコード
+at=np.transpose(a) # 信号の分離
 
 # 時間幅dtを設定
 dt = 1  # 仮に1とします（実際のデータに合わせて変更してください）
 
 # 相互相関を計算
-cross_correlation = np.correlate(a[0], a[4], mode='full')
+cross_correlation = np.correlate(a[0], a[4], mode='full')  # ０番と４番の相互相関
 
 # 時間軸を設定
 #time_axis = np.arange(-len(data_A) + 1, len(data_A), 1) * dt
--- plot_csv.py
+++ plot_csv.py
@@ -7,9 +7,8 @@ from numpy import genfromtxt # csv reading
 #data_A = np.array([0, 0, 1, 0, 0, 1, 0, 0, 1, 0])
 #data_B = np.array([0, 0, 0, 1, 0, 0, 1, 0, 0, 1])
 # read csv with numpy
-a=genfromtxt(sys.argv[1], delimiter=',')
-#print(a[0])
-at=np.transpose(a)
+a=genfromtxt(sys.argv[1], delimiter=',') # CSVデータの読み込み 1レコード＝Tc x 10 , 10レコード
+at=np.transpose(a) # 信号の分離
 
 # 時間幅dtを設定
 dt = 1  # 仮に1とします（実際のデータに合わせて変更してください）
@@ -17,7 +16,7 @@ dt = 1  # 仮に1とします（実際のデータに合わせて変更してく
 # 時間軸を設定
 x=range(0,10)
 
-# plot 10 signals
+# １０個の信号のプロット：山が０から１０まで順番に並ぶ
 for i in range(0,10):
   plt.plot(x, at[i])
 plt.show()
